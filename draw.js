c8c9c6d86630edbaa298dbe900bce1da839a8126
Fadi-Faiyad <zenfaiyad@gmail.com>
2023-04-15 11:35:14 -0800

    Adding and rendering a new view

--- draw.js
+++ draw.js
@@ -1,11 +1,37 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
-function drawing (){
+const CANVAS_WIDTH = canvas.width = 150;
+const CANVAS_HEIGHT = canvas.height = 150;
+var ballSize = 4;
+// x
+var placeSide = canvas.width / 2;
+// y
+var placeTop = canvas.height - 30;
+
+var moveSide = 2;
+var moveTop = -2;
+
+function drawingBall() {
     ctx.beginPath();
-    ctx.arc(50, 50, 10, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
+    ctx.arc(placeSide, placeTop, ballSize, 0, Math.PI * 2);
+    ctx.fillStyle = "red";
     ctx.fill();
     ctx.closePath();
+
 }
-setInterval(drawing, 10)
+
+function moveBall() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawingBall();
+    if (placeSide + moveSide > canvas.width-ballSize || placeSide + moveSide < ballSize){
+        moveSide = -moveSide;
+
+    }
+    if (placeTop + moveTop > canvas.height-ballSize || placeTop + moveTop < ballSize){
+        moveTop = -moveTop;
+    }
+    placeSide += moveSide;
+    placeTop += moveTop;
+}
+setInterval(moveBall, 19)
